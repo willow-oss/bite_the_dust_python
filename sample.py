6ea48716c1dee98a3c8ccc4470d570e216f0672a
willow-oss <58943165+willow-oss@users.noreply.github.com>
2019-12-16 23:43:44 +0900

    added sample for xgboost

--- sample.py
+++ sample.py
@@ -6,8 +6,8 @@ Created on Mon Dec 16 18:46:09 2019
 """
 
 from LoadTestData import TestData
-from LearningAlgos import ExtraTrees
-from HypOptParams import ETOpt
+from LearningAlgos import ExtraTrees, GBDT_XGB
+from HypOptParams import ETOpt, XGBOpt
 from sklearn.model_selection import train_test_split
 
 b = TestData.boston_data_df()
@@ -18,9 +18,24 @@ y = df["PRICE"]
 
 tr_X, va_X, tr_y, va_y = train_test_split(X, y, train_size = 0.9)
 
+
+"""
+#Extra Trees test mode
 ETR = ExtraTrees.ETReg()
 base_params = ETR.base_params
 
 ETO = ETOpt.ETRegOpt()
 best_params = ETO.fetch_best_params(tr_X, va_X, tr_y, va_y)
 print(best_params)
+"""
+
+"""
+#xgboost test mode
+XGBR = GBDT_XGB.XGBReg()
+base_params = XGBR.base_params
+
+XGBO = XGBOpt.XGBRegOpt()
+best_params = XGBO.fetch_best_params(tr_X, va_X, tr_y, va_y)
+XGBR = GBDT_XGB.XGBReg(best_params)
+XGBR.train_and_predict(tr_X, va_X, tr_y, va_y, plot_bool = True)
+"""
